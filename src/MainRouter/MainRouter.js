f588a9be44a25cf47400bf8b3ccd33c66d5413bc
Timothy Do <tydo83@gmail.com>
2021-08-09 14:54:47 -0400

    moved mainRouter comp into app.js

--- src/MainRouter/MainRouter.js
+++ src/MainRouter/MainRouter.js
@@ -2,5 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 export default function MainRouter() {
-	return <div></div>;
+	return <div>
+
+
+
+
+	</div>;
 }
