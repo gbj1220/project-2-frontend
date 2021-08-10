10e396c1f03f153f22dc015d8e2f7759773299a9
Gregory.Johnson <3531op@gmail.com>
2021-08-10 15:52:35 -0400

    Completed application form component

--- src/components/Apply/Apply.js
+++ src/components/Apply/Apply.js
@@ -1,5 +1,116 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-export default function Apply() {
-	return <div>Apply</div>;
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		marginTop: theme.spacing(15),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(15),
+	},
+	submit: {
+		margin: theme.spacing(5, 0, 2),
+	},
+}));
+
+export default function SignUp() {
+	const classes = useStyles();
+
+	return (
+		<Container component='main' maxWidth='xs'>
+			<CssBaseline />
+			<div className={classes.paper}>
+				<Typography component='h1' variant='h5'>
+					Driver Application
+				</Typography>
+				<form className={classes.form} noValidate>
+					<Grid container spacing={5}>
+						<Grid item xs={12}>
+							<TextField
+								autoComplete='fname'
+								name='firstName'
+								variant='outlined'
+								required
+								fullWidth
+								id='firstName'
+								label='First Name'
+								autoFocus
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant='outlined'
+								required
+								fullWidth
+								id='lastName'
+								label='Last Name'
+								name='lastName'
+								autoComplete='lname'
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant='outlined'
+								required
+								fullWidth
+								id='dateOfBirth'
+								label='Date of Birth'
+								name='dateOfBirth'
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant='outlined'
+								required
+								fullWidth
+								name='city'
+								label='City'
+								id='city'
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant='outlined'
+								required
+								fullWidth
+								name='email'
+								label='Email'
+								id='email'
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								variant='outlined'
+								required
+								fullWidth
+								name='phoneNumber'
+								label='Phone Number'
+								id='phoneNumber'
+							/>
+						</Grid>
+					</Grid>
+					<Button
+						type='submit'
+						variant='contained'
+						color='primary'
+						className={classes.submit}
+					>
+						Apply Now
+					</Button>
+				</form>
+			</div>
+		</Container>
+	);
 }
